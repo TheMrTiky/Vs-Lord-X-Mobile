7da7441e3f73af60a7ded055e916af0cef4033c6
TheTiky2 <96601250+TheTiky2@users.noreply.github.com>
2021-12-25 12:15:23 +0000

    Update Achievements.hx

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -26,7 +26,8 @@ class Achievements {
 		["Hyperactive",					"Finish a Song without going Idle.",				'hype',					false],
 		["Just the Two of Us",			"Finish a Song pressing only two keys.",			'two_keys',				false],
 		["Toaster Gamer",				"Have you tried to run the game on a toaster?",		'toastie',				false],
-		["Debugger",					"Beat the \"Test\" Stage from the Chart Editor.",	'debugger',				 true]
+		["Debugger",					"Beat the \"Test\" Stage from the Chart Editor.",	'debugger',				 true],
+                ["X God", "Beat Lord X on X Mode with no miss.", 'weekx_nomiss', false]
 	];
 	public static var achievementsMap:Map<String, Bool> = new Map<String, Bool>();
 
